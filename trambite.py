b737bd276fbbcf9864999b03625d44d7091af93c
Kennedydaniel12 <danninheira753@hotmail.com>
2023-11-14 00:16:55 -0300

    att

--- trambite.py
+++ trambite.py
@@ -1,7 +1,7 @@
 import serial
 import os
 
-arduino = serial.Serial('COM3', 9600, timeout=5)
+arduino = serial.Serial('COM3', 9600, timeout=1)
 
 last_status = ""
 
@@ -10,13 +10,20 @@ try:
         data = arduino.readline().decode().strip()
         print(f"Recebido: {data}")
 
-        if "ABRIR_COBERTURA" in data:
+        if "Telhado Aberto: 1" in data and last_status != "Status do Telhado: 1":
             last_status = "Status do Telhado: 1"
-        elif "FECHAR_COBERTURA" in data:
+        elif "Telhado Aberto: 0" in data and last_status != "Status do Telhado: 0":
             last_status = "Status do Telhado: 0"
 
-        with open('dadosArduino.txt', 'w') as file:
-            file.write(last_status)
+        # Verifica se houve alteração no status antes de escrever no arquivo
+        with open('dadosArduino.txt', 'r') as file:
+            current_status = file.read().strip()
+
+        if current_status != last_status:
+            with open('dadosArduino.txt', 'w') as file:
+                file.write(last_status)
+                print(f"Arquivo atualizado: {last_status}")
+
 except KeyboardInterrupt:
     print("Programa interrompido pelo usuário.")
 finally:
